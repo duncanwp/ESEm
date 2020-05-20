22a3483014cda699d8e26f7e184effc8045a37b3
Duncan Watson-Parris <duncan@watson-parris.co.uk>
2020-05-20 14:36:45 +0100

    Factored out the implausibility and added a batch calculation routine. Further tests

--- GCEm/gp_model.py
+++ GCEm/gp_model.py
@@ -133,6 +133,7 @@ def tf_tqdm(ds):
     return ds.map(advance_tqdm)
 
 
+@tf.function
 def constrain(implausibility, tolerance=0., threshold=3.0):
     """
         Return a boolean array indicating if each sample meets the implausibility criteria:
@@ -146,12 +147,54 @@ def constrain(implausibility, tolerance=0., threshold=3.0):
     """
     # Return True (for a sample) if the number of implausibility measures greater
     #  than the threshold is less than or equal to the tolerance
-    return np.less_equal(np.sum(np.greater(implausibility, threshold), axis=0), tolerance)
+    return tf.less_equal(
+                tf.reduce_sum(tf.cast(tf.greater(implausibility, threshold), dtype=implausibility.dtype), axis=1),
+                tf.multiply(tolerance, tf.cast(tf.shape(implausibility)[1], implausibility.dtype))
+           )
 
 
 def get_implausibility(model, obs, sample_points,
                        obs_uncertainty=0., interann_uncertainty=0.,
-                       repres_uncertainty=0., struct_uncertainty=0., batch_size=1000):
+                       repres_uncertainty=0., struct_uncertainty=0., batch_size=1):
+    """
+
+    Each of the specified uncertainties are assumed to be normal and are added in quadrature
+
+    NOTE - each of the uncertaintes are 1 sigma as compared to previous versions which assumed 2 sigma
+
+    :param model:
+    :param obs:
+    :param sample_points:
+    :param float obs_uncertainty: Fractional, relative (1 sigma) uncertainty in obervations
+    :param float repres_uncertainty: Fractional, relative (1 sigma) uncertainty due to the spatial and temporal
+     representitiveness of the observations
+    :param float interann_uncertainty: Fractional, relative (1 sigma) uncertainty introduced when using a model run
+     for a year other than that the observations were measured in.
+    :param float struct_uncertainty: Fractional, relative (1 sigma) uncertainty in the model itself.
+    :param int batch_size:
+    :return:
+    """
+
+    #TODO: Could add an absolute uncertainty term here
+
+    # Get the square of the absolute uncertainty and broadcast it across the batch (since it's the same for each sample)
+    observational_var = np.broadcast_to(np.square(obs.data * obs_uncertainty), (batch_size, obs.shape[0]))
+    respres_var = np.broadcast_to(np.square(obs.data * repres_uncertainty), (batch_size, obs.shape[0]))
+    interann_var = np.broadcast_to(np.square(obs.data * interann_uncertainty), (batch_size, obs.shape[0]))
+    struct_var = np.broadcast_to(np.square(obs.data * struct_uncertainty), (batch_size, obs.shape[0]))
+
+    implausibility = _tf_implausibility(model, obs.data, sample_points,
+                                        observational_var, interann_var,
+                                        respres_var, struct_var, batch_size=batch_size)
+    # TODO: I could return this as a cube for easier plotting
+
+    return implausibility
+
+
+def batch_constrain(model, obs, sample_points,
+                       obs_uncertainty=0., interann_uncertainty=0.,
+                       repres_uncertainty=0., struct_uncertainty=0.,
+                       tolerance=0., threshold=3.0, batch_size=1):
     """
 
     Each of the specified uncertainties are assumed to be normal and are added in quadrature
@@ -179,18 +222,71 @@ def get_implausibility(model, obs, sample_points,
     interann_var = np.broadcast_to(np.square(obs * interann_uncertainty), (batch_size, obs.shape[0]))
     struct_var = np.broadcast_to(np.square(obs * struct_uncertainty), (batch_size, obs.shape[0]))
 
-    implausibility = _calc_implausibility(model, obs, sample_points,
-                                          observational_var, interann_var,
-                                          respres_var, struct_var, batch_size=batch_size)
+    valid_samples = _tf_constrain(model, obs, sample_points,
+                                   observational_var, interann_var,
+                                   respres_var, struct_var,
+                                   tolerance=tolerance, threshold=threshold,
+                                   batch_size=batch_size)
     # TODO: I could return this as a cube for easier plotting
 
+    return valid_samples
+
+
+@tf.function
+def _tf_constrain(model, obs, sample_points,
+                  observational_var, interann_var, respres_var, struct_var,
+                  tolerance, threshold, batch_size=1):
+    """
+
+    Each of the specified uncertainties are assumed to be normal and are added in quadrature
+
+    NOTE - each of the uncertaintes are 1 sigma as compared to previous versions which assumed 2 sigma
+
+    :param model:
+    :param Tensor obs:
+    :param Tensor sample_points:
+    :param Tensor observational_var: Variance in obervations
+    :param Tensor respres_var: Fractional, relative (1 sigma) uncertainty due to the spatial and temporal
+     representitiveness of the observations
+    :param Tensor interann_var: Fractional, relative (1 sigma) uncertainty introduced when using a model run
+     for a year other than that the observations were measured in.
+    :param Tensor struct_var: Fractional, relative (1 sigma) uncertainty in the model itself.
+    :param int batch_size:
+    :return:
+    """
+    with tf.device('/gpu:{}'.format(model._GPU)):
+
+        sample_T = tf.data.Dataset.from_tensor_slices(sample_points)
+        dataset = sample_T.batch(batch_size)
+
+        all_valid = tf.zeros((0, ), dtype=tf.bool)
+
+        for data in tf_tqdm(dataset):
+            # Get batch prediction
+            emulator_mean, emulator_var = model._tf_predict(data)
+
+            implausibility = _calc_implausibility(emulator_mean, obs,
+                                                  emulator_var, interann_var,
+                                                  observational_var, respres_var,
+                                                  struct_var)
+
+            valid_samples = constrain(implausibility, tolerance, threshold)
+            all_valid = tf.concat([all_valid, valid_samples], 0)
+
+    return all_valid
+
+
+@tf.function
+def _calc_implausibility(emulator_mean, obs, emulator_var, interann_var, observational_var, respres_var, struct_var):
+    tot_sd = tf.sqrt(tf.add_n([emulator_var, observational_var, respres_var, interann_var, struct_var]))
+    implausibility = tf.divide(tf.abs(tf.subtract(emulator_mean, obs)), tot_sd)
     return implausibility
 
 
 @tf.function
-def _calc_implausibility(model, obs, sample_points,
-                         observational_var, interann_var,
-                         respres_var, struct_var, batch_size=1000):
+def _tf_implausibility(model, obs, sample_points,
+                       observational_var, interann_var,
+                       respres_var, struct_var, batch_size=1):
     """
 
     Each of the specified uncertainties are assumed to be normal and are added in quadrature
@@ -215,15 +311,16 @@ def _calc_implausibility(model, obs, sample_points,
 
         dataset = sample_T.batch(batch_size)
 
-        all_implausibility = tf.zeros((0, ), dtype=tf.bool)
+        all_implausibility = tf.zeros((0, obs.shape[0]), dtype=sample_points.dtype)
 
         for data in tf_tqdm(dataset):
-            # Get batch prediction (Note this isn't called yet)
-            m, v = model.model.predict_y(data)
-            emulator_mean, emulator_var = model.model.likelihood.predict_mean_and_var(m, v)
+            # Get batch prediction
+            emulator_mean, emulator_var = model._tf_predict(data)
 
-            tot_sd = tf.sqrt(tf.add_n([emulator_var, observational_var, respres_var, interann_var, struct_var]))
-            implausibility = tf.divide(tf.abs(tf.subtract(emulator_mean, obs)), tot_sd)
+            implausibility = _calc_implausibility(emulator_mean, obs,
+                                                  emulator_var, interann_var,
+                                                  observational_var, respres_var,
+                                                  struct_var)
 
             all_implausibility = tf.concat([all_implausibility, implausibility], 0)
 
@@ -231,7 +328,7 @@ def _calc_implausibility(model, obs, sample_points,
 
 
 @tf.function
-def sample_mean(model, sample_points, batch_size=1):
+def batch_stats(model, sample_points, batch_size=1):
     with tf.device('/gpu:{}'.format(model._GPU)):
         sample_T = tf.data.Dataset.from_tensor_slices(sample_points)
 
@@ -241,13 +338,12 @@ def sample_mean(model, sample_points, batch_size=1):
         tot_s2 = tf.constant(0., dtype=model.dtype)  # Proportion of valid samples required
 
         for data in tf_tqdm(dataset):
-            # Get batch prediction (Note this isn't called yet)
-            m, v = model.model.predict_y(data)
-            my, yv = model.model.likelihood.predict_mean_and_var(m, v)
+            # Get batch prediction
+            emulator_mean, _ = model._tf_predict(data)
 
             # Get sum of x and sum of x**2
-            tot_s += tf.reduce_sum(my, axis=0)
-            tot_s2 += tf.reduce_sum(tf.square(my), axis=0)
+            tot_s += tf.reduce_sum(emulator_mean, axis=0)
+            tot_s2 += tf.reduce_sum(tf.square(emulator_mean), axis=0)
 
     n_samples = tf.cast(sample_points.shape[0], dtype=model.dtype)  # Make this a float to allow division
     # Calculate the resulting first two moments
--- tests/test_gp_model.py
+++ tests/test_gp_model.py
@@ -67,32 +67,114 @@ class Simple2DTest(unittest.TestCase, GPTest):
 
 class SampleTest(unittest.TestCase):
 
-    def test_sample_mean(self):
+    def setUp(self) -> None:
+        self.training_params = get_uniform_params(2)
+        self.training_ensemble = get_1d_two_param_cube(self.training_params)
+
+        self.m = GPModel(self.training_ensemble)
+        self.m.train(self.training_params)
+
+    def test_batch_stats(self):
         # Test that the sample_mean function returns the mean of the sample
-        from GCEm.gp_model import sample_mean
-
-        training_params = get_uniform_params(2)
-
-        training_ensemble = get_1d_two_param_cube(training_params)
-
-        m = GPModel(training_ensemble)
-        m.train(training_params, verbose=True)
+        from GCEm.gp_model import batch_stats
 
         sample_params = get_random_params(2)
         expected_ensemble = get_1d_two_param_cube(sample_params)
 
-        mean, std_dev = sample_mean(m, sample_params)
+        mean, std_dev = batch_stats(self.m, sample_params)
 
         assert_allclose(mean, expected_ensemble.data.mean(axis=0), rtol=1e-1)
         assert_allclose(std_dev, expected_ensemble.data.std(axis=0), rtol=1e-1)
 
-    def test_implausibility(self):
+    def test_implausibility_scalar_uncertainty(self):
         # Test the implausibility is correct
         from GCEm.gp_model import get_implausibility
 
+        obs_uncertainty = 5.
+        # Perturbing the obs by one sd should lead to an implausibility of 1.
+        obs = self.training_ensemble[10].copy() + obs_uncertainty
+
+        # Calculate the implausbility of the training points from a perturbed
+        #  training point. The emulator variance should be zero making testing
+        #  easier.
+        implausibility = get_implausibility(self.m, obs, self.training_params,
+                                            obs_uncertainty=obs_uncertainty/obs.data.mean(),
+                                            interann_uncertainty=0.,
+                                            repres_uncertainty=0.,
+                                            struct_uncertainty=0.)
+
+        # The implausibility for the 10th sample (the one we perturbed around)
+        #  should be one - on average
+        assert_allclose(implausibility.numpy()[10, :].mean(), 1., rtol=1e-2)
+
+    def test_implausibility_vector_uncertainty(self):
+        # Test with a vector obs uncertainty
+        from GCEm.gp_model import get_implausibility
+        obs_uncertainty = self.training_ensemble.data.std(axis=0)
+
+        # Perturbing the obs by one sd should lead to an implausibility of 1.
+        obs = self.training_ensemble[10].copy() + obs_uncertainty
+
+        # Calculate the implausbility of the training points from a perturbed
+        #  training point. The emulator variance should be zero making testing
+        #  easier.
+        implausibility = get_implausibility(self.m, obs, self.training_params,
+                                            obs_uncertainty=obs_uncertainty/obs.data,
+                                            interann_uncertainty=0.,
+                                            repres_uncertainty=0.,
+                                            struct_uncertainty=0.)
+
+        # The implausibility for the 10th sample (the one we perturbed around)
+        #  should be one.
+        expected = np.ones((100,))
+        # The first element has zero variance so nan implausibility
+        expected[0] = np.nan
+        assert_allclose(implausibility.numpy()[10, :], expected, rtol=1e-1)
+
+    def test_calc_implausibility(self):
+        # Test the implausibility is correct
+        from GCEm.gp_model import _calc_implausibility
+
+        # Test a bunch of simple cases
+        imp = _calc_implausibility(np.asarray([1., 1., 2., 1., -2.]),  # Emulator Mean
+                                   np.asarray([1., 1., 1., 2., 1.]),  # Obs
+                                   np.asarray([1., 2., 1., 1., 1.]),  # Emulator var
+                                   np.asarray([1., 1., 1., 1., 1.]),  # Interann var
+                                   np.asarray([1., 1., 1., 1., 1.]),  # Obs var
+                                   np.asarray([1., 1., 1., 1., 1.]),  # respres var
+                                   np.asarray([1., 1., 1., 1., 1.]),  # Struct var
+                                   )
+        assert_allclose(imp, np.asarray([0., 0., 1./np.sqrt(5.), 1./np.sqrt(5.), 3./np.sqrt(5.)]))
+
+        # Test single value inputs
+        imp = _calc_implausibility(np.asarray([1., ]),  # Emulator Mean
+                                   np.asarray([1., ]),  # Obs
+                                   np.asarray([1., ]),  # Emulator var
+                                   np.asarray([1., ]),  # Interann var
+                                   np.asarray([1., ]),  # Obs var
+                                   np.asarray([1., ]),  # respres var
+                                   np.asarray([1., ]),  # Struct var
+                                   )
+        assert_allclose(imp, np.asarray([0.]))
+
+        # Test invalid inputs
+        imp = _calc_implausibility(np.asarray([1., ]),  # Emulator Mean
+                                   np.asarray([1., ]),  # Obs
+                                   np.asarray([0., ]),  # Emulator var
+                                   np.asarray([0., ]),  # Interann var
+                                   np.asarray([0., ]),  # Obs var
+                                   np.asarray([0., ]),  # respres var
+                                   np.asarray([0., ]),  # Struct var
+                                   )
+        assert_allclose(imp, np.asarray([np.nan]))
+
     def test_constrain(self):
         # Test that constrain returns the correct boolean array for the given implausibiltiy and params
-        pass
+        from GCEm.gp_model import get_implausibility, constrain
+
+    def test_batch_constrain(self):
+        # Test that constrain returns the correct boolean array for the given implausibiltiy and params
+        from GCEm.gp_model import get_implausibility, batch_constrain
 
 
 if __name__ == '__main__':
