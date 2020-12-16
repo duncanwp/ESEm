b599003c4caf2c6d1e70adafd93f316e0690dbeb
Andrew Williams <williamsa3142@gmail.com>
2020-12-16 15:16:57 +0000

    Added exmaple notebook and utils for RF

---
+++ examples/utils_rf.py
@@ -0,0 +1,131 @@
+"""
+===================================================================
+July 2020 --- Andrew Williams
+===================================================================
+Utility functions for plotting and analysis
+===================================================================
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def add_121_line(ax):
+    import numpy as np
+    lims = [
+        np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
+        np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
+    ]
+
+    # now plot both limits against eachother
+    ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
+    ax.set_xlim(lims)
+    ax.set_ylim(lims)
+    
+def prettify_plot(ax):
+    """utility function for making plots prettier"""
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    ax.spines['left'].set_position(('outward', 10))
+    ax.spines['bottom'].set_position(('outward', 10))
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    
+    
+def plot_results(ax, truth, pred, title):
+    from sklearn.metrics import median_absolute_error, r2_score, mean_squared_error
+    from sklearn.model_selection import train_test_split
+    ax.scatter(y=pred, x=truth, zorder=2.6, s=20, alpha=0.5)
+    props = {'boxstyle':'round', 'facecolor':'wheat'}
+    ax.text(x=0.05, y=0.8, transform=ax.transAxes,
+            s=f'$R^2$={np.round(r2_score(pred, truth),2)}, ' +\
+                f'MAE={np.round(median_absolute_error(pred, truth),3)}, ' +\
+                f'RMSE={np.round( np.sqrt(mean_squared_error(pred, truth)), 2)}',
+            bbox=props)
+
+    # Change axis formatting
+    prettify_plot(ax)
+    add_121_line(ax)
+    ax.set_title(title)
+    ax.set_xlabel('Truth')
+    ax.set_ylabel('Prediction')
+    
+    
+def LeaveOneOut(Xdata, Ydata, rndseed=0, **rf_kwargs):
+    """
+    Function to perform LeaveOneOut cross-validation with different estimators. 
+    """
+    from GCEm.rf_model import RFModel
+    
+    # fix random seed for reproducibility 
+    # then shuffle X,Y indices so that they're not ordered (in time, for example)
+    np.random.seed(rndseed)
+    rndperm = np.random.permutation(Xdata.shape[0])
+    
+    # How many indices?
+    n_data = Xdata.shape[0]
+    
+    # Output array for test val, pred_mean
+    output = np.vstack([np.empty(2) for _ in range(n_data)])
+    
+    for test_idx in range(n_data):
+        """Split into training - validation sets"""
+        X_train = Xdata.iloc[rndperm[np.arange(len(rndperm))!= test_idx], :]
+        X_test  = Xdata.iloc[rndperm[test_idx], :].to_numpy().reshape(1,-1)
+        
+        Y_train = Ydata[rndperm[np.arange(len(rndperm))!= test_idx]]
+        Y_test  = Ydata[rndperm[test_idx]]
+       
+        """Construct and fit model"""
+        model = RFModel(training_params=X_train, 
+                        training_data=Y_train, 
+                        random_state=rndseed, 
+                        **rf_kwargs)
+        
+        model.train()
+            
+        """Evaluate model on test data"""
+        predictions,v = model.predict(X_test)
+        
+        """Save output for validation plot  later on"""
+        output[test_idx] = (Y_test, predictions)
+
+    return output
+
+def get_crm_data(cache_path='.', preprocess=True):
+    """
+    Load the example cloud-resolving model data, download if not present.
+    :param str cache_path: Path to load/store the data
+    :param bool preprocess: Whether or not to clean and concatenate the data
+    :return:
+    """
+    import pandas as pd
+    import cis
+
+    bc_ppe_cache = os.path.join(cache_path, 'BC_PPE_PD_AAOD_monthly.nc')
+
+    if not os.path.isfile(bc_ppe_cache):
+        urllib.request.urlretrieve("https://zenodo.org/record/4323300/files/BC_PPE_PD_AAOD_monthly.nc?download=1", bc_ppe_cache)
+
+    params_cache = os.path.join(cache_path, 'aerocommmppe_bcdesign.csv')
+    if not os.path.isfile(params_cache):
+        urllib.request.urlretrieve("https://zenodo.org/record/4323300/files/aerocommmppe_bcdesign.csv?download=1", params_cache)
+
+    ppe_params = pd.read_csv(params_cache)
+    ppe_aaod = cis.read_data(bc_ppe_cache, 'ABS_2D_550nm', 'NetCDF_Gridded')
+    # Ensure the job dimension is at the front
+    ppe_aaod.transpose((1, 0, 2, 3))
+    
+    if normalize_params:
+        ppe_params = ppe_params.apply(normalize, axis=0)
+    
+    if dre:
+        ppe_dre_cache = os.path.join(cache_path, 'BC_PPE_PD_FORCING_monthly.nc')
+        if not os.path.isfile(ppe_dre_cache):
+            urllib.request.urlretrieve("https://zenodo.org/record/3856645/files/BC_PPE_PD_FORCING_monthly.nc?download=1", ppe_dre_cache)
+
+        ppe_dre = cis.read_data(ppe_dre_cache, 'FSW_TOTAL_TOP', 'NetCDF_Gridded')        
+        return ppe_params, ppe_aaod, ppe_dre
+    else:
+        return ppe_params, ppe_aaod
