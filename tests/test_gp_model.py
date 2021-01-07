8e98556ea603495a8b6db637ab0d683c3a82c4ae
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2021-01-07 12:43:30 +0000

    Trying to increase jitter for 1d GP tests

--- tests/test_gp_model.py
+++ tests/test_gp_model.py
@@ -81,11 +81,16 @@ class Simple1DTest(unittest.TestCase, GPTest):
 
     @classmethod
     def setUpClass(cls) -> None:
+        from gpflow.config import as_context, Config
+
         params, test = pop_elements(get_uniform_params(2), 10, 12)
 
         ensemble = get_1d_two_param_cube(params)
-        m = GPModel(params, ensemble)
-        m.train()
+
+        config = Config(jitter=1e-4)
+        with as_context(config):
+            m = GPModel(params, ensemble)
+            m.train()
 
         cls.model = m
         cls.params = params
